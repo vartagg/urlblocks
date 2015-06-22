b6c34ea9cd46ead2b0b1e5479b5b69e3a781cbfe
Chub <vartagg@users.noreply.github.com>
2015-06-22 23:58:37 +0700

    Fixes setup.py. setup.cfg added for wheels

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 
 from setuptools import setup, find_packages
 
 version = '0.1'
 
+description = ('urlblocks is the module provided URL string class which can '
+               'be operated as a constructor, consisting of some blocks - URL components.')
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+long_description = read('README.rst')
+
+
+
 setup(
     name='urlblocks',
+    author='Vladimir Chub',
     version=version,
-    description='A utility class for manipulating URLs.',
+    description=description,
     author_email="vartagg@users.noreply.github.com",
     url='http://github.com/vartagg/urlblocks',
     packages=find_packages(exclude=('test',)),
